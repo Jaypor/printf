4f7196b4112280ef043666fe6ca53672aee91c49
Ani peter <benjaminparish6@gmail.com>
2022-10-18 00:46:00 +0100

    added first file

--- print_alphabets.c
+++ print_alphabets.c
@@ -1,4 +1,37 @@
 #include "main.h"
 
 /**
-* 
+*prints an alpha
+*/
+
+int _printf(const char *format, ...)
+{ 
+	char *monitor; 
+	unsigned int i; 
+	char *s; 
+	
+	//Module 1: Initializing a variadic funtion 
+	va_list arg; 
+	va_start(arg, format); 
+	
+	for(monitor = format; *monitor != '\0'; monitor++) 
+	{ 
+		while( *monitor != '%' ) 
+		{ 
+			putchar(*monitor);
+			monitor++; 
+		} 
+		
+		monitor++; 
+		
+		//Module 2: Fetching and executing arguments
+		switch(*monitor) 
+		{ 
+			case 'c' : i = va_arg(arg,int);		//Fetch char argument
+						putchar(i);
+						break;  
+
+			case 's': s = va_arg(arg,char *); 		//Fetch string
+						puts(s); 
+						break;
+}
